6e79789774efdd00c8726d3431ac7cd07843d91a
hzengler <hanszengler@gmail.com>
2023-01-13 16:42:11 +0100

    feat(video): add locale video options

--- okp4-gatsby/src/components/pages/index/Video.js
+++ okp4-gatsby/src/components/pages/index/Video.js
@@ -1,13 +1,15 @@
 import React from "react";
 import IconPlay from "/src/assets/images/icons/play.inline.svg";
 import Strate from "../../animations/Strate.js";
-import VideoYoutube from "../../VideoYoutube.js";
+// import VideoYoutube from "../../VideoYoutube.js";
+import VideoLocale from "../../VideoLocale.js";
 
 const Video = () => {
   return (
     <Strate classContainer={"video"}>
       <div className="video__player" data-reveal="1">
-        <VideoYoutube youtubeId="Dt9O3-1bIDU" />
+        {/* <VideoYoutube youtubeId="Dt9O3-1bIDU" /> */}
+        <VideoLocale />
         <IconPlay />
       </div>
     </Strate>
