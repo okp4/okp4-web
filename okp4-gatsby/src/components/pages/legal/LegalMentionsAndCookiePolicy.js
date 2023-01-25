629f75a143569df4180b6469a067ec75e6bfb8e5
Joey <joey.remion@gmail.com>
2023-01-25 14:11:37 +0100

    feat: improve style

--- okp4-gatsby/src/components/pages/legal/LegalMentionsAndCookiePolicy.js
+++ okp4-gatsby/src/components/pages/legal/LegalMentionsAndCookiePolicy.js
@@ -9,52 +9,56 @@ const LegalMentionsAndCookiePolicy = () => {
       <Halo />
       <div className="wrapper">
         <div className="legalMentions__content">
-          <h3>{contentLegalMentions.title}</h3>
-          <div className="legalMentions__items">
-            {contentLegalMentions.items.map((item, index) => {
-              return (
-                <div
-                  data-key={index}
-                  key={index}
-                  className="legalMentions__item"
-                >
-                  <h4 className="title">{item.title}</h4>
-                  <p
-                    className="text"
-                    dangerouslySetInnerHTML={{
-                      __html: item.text,
-                    }}
-                  ></p>
-                </div>
-              );
-            })}
+          <div>
+            <h3>{contentLegalMentions.title}</h3>
+            <div className="legalMentions__items">
+              {contentLegalMentions.items.map((item, index) => {
+                return (
+                  <div
+                    data-key={index}
+                    key={index}
+                    className="legalMentions__item"
+                  >
+                    <h4 className="title">{item.title}</h4>
+                    <p
+                      className="text"
+                      dangerouslySetInnerHTML={{
+                        __html: item.text,
+                      }}
+                    ></p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <h3>{contentCookiePolicy.title}</h3>
-          <br />
-          <p
-            className="text"
-            dangerouslySetInnerHTML={{
-              __html: contentCookiePolicy.subtitle,
-            }}
-          ></p>
-          <div className="legalMentions__items">
-            {contentCookiePolicy.items.map((item, index) => {
-              return (
-                <div
-                  data-key={index}
-                  key={index}
-                  className="legalMentions__item"
-                >
-                  <h4 className="title">{item.title}</h4>
-                  <p
-                    className="text"
-                    dangerouslySetInnerHTML={{
-                      __html: item.text,
-                    }}
-                  ></p>
-                </div>
-              );
-            })}
+          <div>
+            <h3>{contentCookiePolicy.title}</h3>
+            <br />
+            <p
+              className="subtitle"
+              dangerouslySetInnerHTML={{
+                __html: contentCookiePolicy.subtitle,
+              }}
+            ></p>
+            <div className="legalMentions__items">
+              {contentCookiePolicy.items.map((item, index) => {
+                return (
+                  <div
+                    data-key={index}
+                    key={index}
+                    className="legalMentions__item"
+                  >
+                    <h4 className="title">{item.title}</h4>
+                    <p
+                      className="text"
+                      dangerouslySetInnerHTML={{
+                        __html: item.text,
+                      }}
+                    ></p>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
