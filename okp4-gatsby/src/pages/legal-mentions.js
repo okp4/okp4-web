7768a6da72a257cc97f9967422dd73f525c7d11c
Joey <joey.remion@gmail.com>
2023-01-25 17:14:03 +0100

    feat: add graphql fetch all files

--- okp4-gatsby/src/pages/legal-mentions.js
+++ okp4-gatsby/src/pages/legal-mentions.js
@@ -1,11 +1,30 @@
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 import LegalMentionsAndCookiePolicy from "../components/pages/legal/LegalMentionsAndCookiePolicy";
 
-export default function LegalMentions() {
+export default function LegalMentions({ data }) {
+  const allFiles = data.allFile.edges;
   return (
-    <Layout location={"legalMentions"}>
+    <Layout location={"legalMentions"} files={allFiles}>
       <LegalMentionsAndCookiePolicy />
     </Layout>
   );
 }
+
+export const pageQuery = graphql`
+  query AllImages {
+    allFile {
+      edges {
+        node {
+          id
+          relativePath
+          publicURL
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+      }
+    }
+  }
+`;
