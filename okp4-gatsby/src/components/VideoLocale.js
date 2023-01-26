673f5c41c9062a4f728a803db1451129390ba0c3
hzengler <hanszengler@gmail.com>
2023-01-26 15:29:42 +0100

    feat(video): add lazy loading of locale videos

--- okp4-gatsby/src/components/VideoLocale.js
+++ okp4-gatsby/src/components/VideoLocale.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as MediaManager from "../utils/MediaManager.js";
 
 const VideoLocale = ({ files, mp4, webm }) => {
+  const divRef = useRef(null);
+
+  const revealVideo = () => {
+    var intersectionAppear;
+    var optionsAppear = {
+      root: null,
+      rootMargin: "100px",
+      threshold: 0,
+    };
+    intersectionAppear = new IntersectionObserver(appearInView, optionsAppear);
+    intersectionAppear.observe(divRef.current);
+  };
+
+  const appearInView = (entries, intersectionAppear) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        divRef.current.play();
+        intersectionAppear.unobserve(entry.target);
+      }
+    });
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      revealVideo();
+    }, 1000);
+  });
+
   return (
     <div className="videoWrapper">
-      <video autoPlay loop muted playsInline controls>
+      <video preload="none" loop muted playsInline controls ref={divRef}>
         <source src={MediaManager.GetVideo(mp4, files)} type="video/mp4" />
         <source src={MediaManager.GetVideo(webm, files)} type="video/webm" />
       </video>
