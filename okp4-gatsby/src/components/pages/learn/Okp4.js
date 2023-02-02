3895dca84aabb5ed504c873b020f0f86e788f458
Joey <joey.remion@gmail.com>
2023-02-02 10:08:23 +0100

    refactor: improve naming

--- okp4-gatsby/src/components/pages/learn/Okp4.js
+++ okp4-gatsby/src/components/pages/learn/Okp4.js
@@ -9,24 +9,24 @@ const Okp4 = ({ files }) => (
     <Halo classContainer={"halo--big"}></Halo>
     <GatsbyImage
       className="okp4__presentation_image"
-      image={MediaManager.GetImage(content.presentation.image, files)}
-      alt={content.presentation.title}
+      image={MediaManager.GetImage(content.presentationPart.image, files)}
+      alt={content.presentationPart.title}
     />
     <div className="wrapper">
       <section className="okp4__presentation">
         <div>
-          <h2 className="okp4__title">{content.presentation.title}</h2>
+          <h2 className="okp4__title">{content.presentationPart.title}</h2>
           <div className="okp4__presentation__text">
             <p
               className="okp4__text__primary okp4__presentation__introduction"
               dangerouslySetInnerHTML={{
-                __html: content.presentation.introduction,
+                __html: content.presentationPart.introduction,
               }}
             ></p>
             <p
               className="okp4__text__secondary"
               dangerouslySetInnerHTML={{
-                __html: content.presentation.description,
+                __html: content.presentationPart.description,
               }}
             ></p>
           </div>
@@ -38,16 +38,19 @@ const Okp4 = ({ files }) => (
             <div className="okp4__section__image">
               <GatsbyImage
                 className="imgWrapper"
-                image={MediaManager.GetImage(content.dataSharing.image, files)}
-                alt={content.dataSharing.title}
+                image={MediaManager.GetImage(
+                  content.dataSharingPart.image,
+                  files
+                )}
+                alt={content.dataSharingPart.title}
               />
             </div>
             <div className=" okp4__section__description">
-              <h3 className="okp4__title">{content.dataSharing.title}</h3>
+              <h3 className="okp4__title">{content.dataSharingPart.title}</h3>
               <div
                 className="okp4__text__secondary okp4__text-part"
                 dangerouslySetInnerHTML={{
-                  __html: content.dataSharing.text,
+                  __html: content.dataSharingPart.text,
                 }}
               ></div>
             </div>
@@ -56,30 +59,30 @@ const Okp4 = ({ files }) => (
       </div>
 
       <div className="okp4__know">
-        <h3 className="okp4__know__title">{content.know.title}</h3>
+        <h3 className="okp4__know__title">{content.knowPart.title}</h3>
         <div className="okp4__know__content">
           <div
             className=" okp4__text__primary okp4__know__introduction"
             dangerouslySetInnerHTML={{
-              __html: content.know.introduction,
+              __html: content.knowPart.introduction,
             }}
           ></div>
           <div
             className="okp4__text__secondary okp4__know__description"
             dangerouslySetInnerHTML={{
-              __html: content.know.description,
+              __html: content.knowPart.ecosystemDescription,
             }}
           ></div>
           <div
             className="okp4__know__know okp4__text__emphase"
             dangerouslySetInnerHTML={{
-              __html: content.know.know,
+              __html: content.knowPart.knowDescription,
             }}
           ></div>
           <div className="okp4__know__image okp4__image">
             <GatsbyImage
-              image={MediaManager.GetImage(content.know.image, files)}
-              alt={content.know.title}
+              image={MediaManager.GetImage(content.knowPart.image, files)}
+              alt={content.knowPart.title}
             />
           </div>
         </div>
@@ -87,33 +90,33 @@ const Okp4 = ({ files }) => (
 
       <div className="okp4__section">
         <div className="okp4__section__content okp4__cosmos">
-          <h3 className="okp4__cosmos__title">{content.cosmos.title}</h3>
+          <h3 className="okp4__cosmos__title">{content.cosmosPart.title}</h3>
           <div className="okp4__cosmos__content">
             <div
               className="okp4__text__primary okp4__cosmos__introduction"
               dangerouslySetInnerHTML={{
-                __html: content.cosmos.introduction,
+                __html: content.cosmosPart.introduction,
               }}
             ></div>
             <div>
               <div
                 className="okp4__cosmos__link okp4__text__emphase"
                 dangerouslySetInnerHTML={{
-                  __html: content.cosmos.cosmos,
+                  __html: content.cosmosPart.cosmosLink,
                 }}
               ></div>
             </div>
             <div
               className="okp4__cosmos__description okp4__text__secondary"
               dangerouslySetInnerHTML={{
-                __html: content.cosmos.description,
+                __html: content.cosmosPart.description,
               }}
             ></div>
 
             <div className="okp4__cosmos__image okp4__image">
               <GatsbyImage
-                image={MediaManager.GetImage(content.cosmos.image, files)}
-                alt={content.cosmos.title}
+                image={MediaManager.GetImage(content.cosmosPart.image, files)}
+                alt={content.cosmosPart.title}
               />
             </div>
           </div>
