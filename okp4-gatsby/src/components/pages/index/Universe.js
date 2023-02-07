19b8c53348f5f81af26d5d77b08be271366a43fd
ErikssonJoakim <j.eriksson@okp4.com>
2023-02-07 18:16:07 +0100

    style(homepage): precise homepage roadmap class names

--- okp4-gatsby/src/components/pages/index/Universe.js
+++ okp4-gatsby/src/components/pages/index/Universe.js
@@ -12,7 +12,9 @@ const Universe = ({ files }) => {
   const roadmapItems = useRef(null);
 
   const setViewMore = () => {
-    let items = roadmapItems.current.querySelectorAll(".roadmap__item__text");
+    let items = roadmapItems.current.querySelectorAll(
+      ".homepage__roadmap__item__text"
+    );
 
     items.forEach((item) => {
       let rectHeight = item.getBoundingClientRect().height;
@@ -43,7 +45,7 @@ const Universe = ({ files }) => {
   useEffect(() => {
     if (ResponsiveManager.isWindowSmaller("sm")) {
       let items = roadmapItems.current.querySelectorAll(
-        ".roadmap__item__readMore"
+        ".homepage__roadmap__item__readMore"
       );
       items.forEach((item) => {
         item.addEventListener("click", viewMoreText);
@@ -105,9 +107,11 @@ const Universe = ({ files }) => {
             </div>
           </StickyUniverse>
 
-          <div className="roadmap">
-            <div className="roadmap__intro">
-              <h2 className="roadmap__title">{contentRoadmap.title}</h2>
+          <div className="homepage__roadmap">
+            <div className="homepage__roadmap__intro">
+              <h2 className="homepage__roadmap__title">
+                {contentRoadmap.title}
+              </h2>
               <p className="title">{contentRoadmap.subtitle}</p>
               <p
                 className="text"
@@ -116,7 +120,7 @@ const Universe = ({ files }) => {
                 }}
               ></p>
             </div>
-            <div className="roadmap__items" ref={roadmapItems}>
+            <div className="homepage__roadmap__items" ref={roadmapItems}>
               {contentRoadmap.items.map((item, index) => {
                 let topRoadmapSticky = 20 + index * 30;
                 const scaleSteps = contentRoadmap.items.length - 1;
@@ -125,18 +129,18 @@ const Universe = ({ files }) => {
                 let scaleMax = 0.2 - scaleStep * index;
                 return (
                   <StrateRoadmap
-                    classContainer={"roadmap__item"}
+                    classContainer={"homepage__roadmap__item"}
                     scaleTopSticky={topRoadmapSticky}
                     scaleMax={scaleMax}
                     key={index}
                     isLastItem={isLastItem}
                   >
-                    <div className="roadmap__item__layer"></div>
-                    <div className="roadmap__item__container">
-                      <span className="roadmap__item__text--mobile">
+                    <div className="homepage__roadmap__item__layer"></div>
+                    <div className="homepage__roadmap__item__container">
+                      <span className="homepage__roadmap__item__text--mobile">
                         — {item.title}
                       </span>
-                      <div className="roadmap__item__text">
+                      <div className="homepage__roadmap__item__text">
                         <span className="title">— {item.title}</span>
                         <p className="excerpt">{item.subtitle}</p>
                         <p className="description">
@@ -146,14 +150,14 @@ const Universe = ({ files }) => {
                               __html: item.description,
                             }}
                           ></span>
-                          <span className="roadmap__item__readMore">
+                          <span className="homepage__roadmap__item__readMore">
                             <span className="readMore--closed">Read more</span>
                             <span className="readMore--opened">Read less</span>
                           </span>
                         </p>
                       </div>
-                      <div className="roadmap__item__illus">
-                        <div className="roadmap__item__date">
+                      <div className="homepage__roadmap__item__illus">
+                        <div className="homepage__roadmap__item__date">
                           <span>{item.tag}</span>
                         </div>
                         <GatsbyImage
