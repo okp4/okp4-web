a3bf3c455a6a1b4720d9e7dcb9fab84ee7c59d85
ErikssonJoakim <j.eriksson@okp4.com>
2023-02-07 18:32:06 +0100

    feat(roadmap): add closed card structure with style

--- okp4-gatsby/src/components/pages/learn/Roadmap.js
+++ okp4-gatsby/src/components/pages/learn/Roadmap.js
@@ -2,6 +2,8 @@ import React from "react";
 import contentRoadmap from "/content/pages/learn/roadmap.yaml";
 import Halo from "../../animations/Halo.js";
 import { StaticImage } from "gatsby-plugin-image";
+import classNames from "classnames";
+import ExpandIcon from "../../../assets/images/icons/expand.inline.svg";
 
 const Roadmap = () => {
   return (
@@ -12,7 +14,23 @@ const Roadmap = () => {
         alt="background image"
         className="roadmap__background__image"
       />
-      <h1 className="roadmap__title">{contentRoadmap.title}</h1>
+      <div className="roadmap__content__wrapper">
+        <h1 className="roadmap__title">{contentRoadmap.title}</h1>
+        <div className="roadmap__cards">
+          {cards.map(({ title, period }) => (
+            <div className={classNames("roadmap__card", title)} key={title}>
+              <p className="roadmap__card__period">{period}</p>
+              <div className="roadmap__card__content__wrapper">
+                <p className="roadmap__card__title">{`- ${title}`}</p>
+                <button className="roadmap__card__button">
+                  Discover
+                  <ExpandIcon />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
