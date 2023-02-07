1bae71b79644f179eddfb84b54cd7e5e7f106a68
ErikssonJoakim <j.eriksson@okp4.com>
2023-02-07 18:06:12 +0100

    feat(roadmap): init page

---
+++ okp4-gatsby/src/components/pages/learn/Roadmap.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Halo from "../../animations/Halo.js";
+
+const Roadmap = () => {
+  return (
+    <div className="roadmap__main">
+      <Halo />
+    </div>
+  );
+};
+
+export default Roadmap;
