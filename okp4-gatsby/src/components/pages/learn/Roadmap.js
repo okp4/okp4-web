517c82f693a667c8f083334596f398d323528ca4
ErikssonJoakim <j.eriksson@okp4.com>
2023-02-07 18:23:59 +0100

    feat(roadmap): add page background image

--- okp4-gatsby/src/components/pages/learn/Roadmap.js
+++ okp4-gatsby/src/components/pages/learn/Roadmap.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Halo from "../../animations/Halo.js";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Roadmap = () => {
   return (
     <div className="roadmap__main">
       <Halo />
+      <StaticImage
+        src="../../../assets/images/illus/background_img_roadmap.png"
+        alt="background image"
+        className="roadmap__background__image"
+      />
     </div>
   );
 };
