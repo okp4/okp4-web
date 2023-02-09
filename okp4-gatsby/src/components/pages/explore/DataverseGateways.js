e9ffb1cce7bc9dbda62eaed5ca820ff0b686a249
Joey <joey.remion@gmail.com>
2023-02-09 16:43:39 +0100

    feat(dataverse gateways): add scrollable keywords
    Add the scrollable keywords part without the scrol control

--- okp4-gatsby/src/components/pages/explore/DataverseGateways.js
+++ okp4-gatsby/src/components/pages/explore/DataverseGateways.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { useEffect, useState, useRef, useCallback } from "react";
+import { motion } from "framer-motion";
 import { GatsbyImage } from "gatsby-plugin-image";
 import * as MediaManager from "../../../utils/MediaManager.js";
 import Halo from "../../animations/Halo.js";
@@ -45,49 +47,125 @@ const cardStyles = [
   },
 ];
 
-const DataverseGateways = ({ files }) => (
-  <div className="dataverse_gateways">
-    <Halo />
-    <div className="wrapper">
-      <div className="dg__first_page">
-        <h1 className="dg__main_title">{content.title}</h1>
-        <div className="dg__keywords__container"></div>
-      </div>
-      <div className="dg__landscape">
-        <h2>{content.landscape.title}</h2>
-        <p className="dg__landscape__introduction">
-          {content.landscape.introduction}
-        </p>
+const Keyword = ({ keywords, mainIndex, distance }) => (
+  <div className={`keyword`}>
+    {keywords[(keywords.length + mainIndex + distance) % keywords.length]}
+  </div>
+);
 
-        <div className="dg__landscape__cards">
-          {content.landscape.cards.map((card, index) => (
-            <Parallax
-              classContainer="dg__landscape__cards__item"
-              parallaxStart={parallaxLandscapeCard[index]}
-              parallaxEnd={-parallaxLandscapeCard[index]}
-              parallaxMiddle="0.5"
-            >
-              <div
-                className="dg__landscape__cards__item__image"
-                data-infinite="1"
-              >
-                <GatsbyImage
-                  image={MediaManager.GetImage("explore/" + card.image, files)}
-                  alt={card.title}
-                />
+const DataverseGateways = ({ files }) => {
+  const ref = useRef(null);
+  const [scrollState, setScrollState] = useState("scrollWords");
+  const [mainKeywordIndex, setMainKeywordIndex] = useState(0);
+  const keywords = content.featured.keywords;
+
+  const handleWheel = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      if (e.deltaY < 0 && mainKeywordIndex > 0) {
+        setMainKeywordIndex(mainKeywordIndex - 1);
+      } else if (e.deltaY > 0 && mainKeywordIndex < keywords.length - 1) {
+        setMainKeywordIndex(mainKeywordIndex + 1);
+      }
+    },
+    [keywords, mainKeywordIndex, setMainKeywordIndex]
+  );
+
+  useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.addEventListener("wheel", handleWheel);
+    }
+    // if (scrollState === "scrollPage") {
+    //   ref?.current?.removeEventListener("wheel", handleWheel);
+    // }
+    return () => {
+      ref?.current?.removeEventListener("wheel", handleWheel);
+    };
+  }, [ref, handleWheel]);
+
+  return (
+    <div className="dataverse_gateways" ref={ref}>
+      <Halo />
+      <div className="wrapper">
+        <div className="dg__first_page">
+          <h1 className="dg__main_title">{content.title}</h1>
+          <div className="dg__keywords">
+            <div className="dg__keywords__container">
+              <p className="introduction">{content.featured.introduction}</p>
+              <div className="keywords--container">
+                <div className="keywords">
+                  <div className="keywords--aside keywords--previous">
+                    {[-3, -2, -1].map((distance) => (
+                      <Keyword
+                        key={distance}
+                        keywords={keywords}
+                        mainIndex={mainKeywordIndex}
+                        distance={distance}
+                      />
+                    ))}
+                  </div>
+                  <div>
+                    <Keyword
+                      keywords={keywords}
+                      mainIndex={mainKeywordIndex}
+                      distance={0}
+                    />
+                  </div>
+                  <div className="keywords--aside keywords--next">
+                    {[1, 2, 3].map((distance) => (
+                      <Keyword
+                        key={distance}
+                        keywords={keywords}
+                        mainIndex={mainKeywordIndex}
+                        distance={distance}
+                      />
+                    ))}
+                  </div>
+                </div>
               </div>
-              <div className="dg__landscape__cards__item__text">
-                <p className="title">{card.title}</p>
-                <p className="text">{card.text}</p>
-              </div>
-            </Parallax>
-          ))}
+            </div>
+          </div>
         </div>
+
+        <div className="dg__landscape">
+          <h2>{content.landscape.title}</h2>
+          <p className="dg__landscape__introduction">
+            {content.landscape.introduction}
+          </p>
+
+          <div className="dg__landscape__cards">
+            {content.landscape.cards.map((card, index) => (
+              <Parallax
+                key={index}
+                classContainer="dg__landscape__cards__item"
+                parallaxStart={parallaxLandscapeCard[index]}
+                parallaxEnd={-parallaxLandscapeCard[index]}
+                parallaxMiddle="0.5"
+                size="xxl"
+              >
+                <div className="dg__landscape__cards__item__image">
+                  <GatsbyImage
+                    image={MediaManager.GetImage(
+                      "explore/" + card.image,
+                      files
+                    )}
+                    alt={card.title}
+                  />
+                </div>
+                <div className="dg__landscape__cards__item__text">
+                  <p className="title">{card.title}</p>
+                  <p className="text">{card.text}</p>
+                </div>
+              </Parallax>
+            ))}
+          </div>
 
           <div className="dg__protocol">
             <div className="dg__protocol__cards_container">
               {content.protocol.cards.map((card, index) => (
                 <motion.div
+                  key={index}
                   className="dg__protocol__card"
                   whileHover={{ scale: 1.7 }}
                   transition={{ duration: 0.2 }}
@@ -155,68 +233,6 @@ const DataverseGateways = ({ files }) => (
       </div>
     </div>
   );
-        <div className="dg__protocol">
-          <div className="dg__protocol__cards_container">
-            {content.protocol.cards.map((card) => (
-              <div className="dg__protocol__card">
-                <GatsbyImage
-                  image={MediaManager.GetImage("explore/" + card, files)}
-                  alt={card}
-                />
-              </div>
-            ))}
-          </div>
-          <div className="dg__protocol__book">
-            <div className="dg__protocol__book--element">
-              <GatsbyImage
-                image={MediaManager.GetImage(
-                  "explore/protocol-book.png",
-                  files
-                )}
-                alt="OKP4 Protocol"
-              />
-            </div>
-            <div className="dg__protocol__book--element page page1">
-              <GatsbyImage
-                image={MediaManager.GetImage(
-                  "explore/protocol-page-lb.png",
-                  files
-                )}
-                alt="OKP4 Protocol"
-              />
-            </div>
-            <div className="dg__protocol__book--element page page2">
-              <GatsbyImage
-                image={MediaManager.GetImage(
-                  "explore/protocol-page-lt.png",
-                  files
-                )}
-                alt="OKP4 Protocol"
-              />
-            </div>
-            <div className="dg__protocol__book--element page page3">
-              <GatsbyImage
-                image={MediaManager.GetImage(
-                  "explore/protocol-page-rb.png",
-                  files
-                )}
-                alt="OKP4 Protocol"
-              />
-            </div>
-            <div className="dg__protocol__book--element page page4">
-              <GatsbyImage
-                image={MediaManager.GetImage(
-                  "explore/protocol-page-rt.png",
-                  files
-                )}
-                alt="OKP4 Protocol"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+};
 
 export default DataverseGateways;
