8a8ebb2e906a0126814a69d3d5172305875d3abe
lauren olivier <laurenzolivier@gmail.com>
2023-02-17 15:55:57 +0100

    feat: create contact page

---
+++ okp4-gatsby/src/components/pages/contact/Contact.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Halo from "../../animations/Halo.js";
+
+const Contact = () => (
+  <div className="contact-okp4">
+    <Halo classContainer={"halo--big"}></Halo>
+    <div className="contact-main">
+      <div className="wrapper">
+        <h1>Contact Page</h1>
+      </div>
+    </div>
+  </div>
+);
+
+export default Contact;
