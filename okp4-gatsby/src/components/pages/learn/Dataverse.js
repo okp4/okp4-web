0accf45c44a174a168e39472f0756f9b60d503f9
Joey <joey.remion@gmail.com>
2023-02-22 08:27:30 +0100

    feat: add empty page learn dataverse

---
+++ okp4-gatsby/src/components/pages/learn/Dataverse.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import * as MediaManager from "../../../utils/MediaManager.js";
+import Halo from "../../animations/Halo.js";
+import content from "/content/pages/learn/dataverse.yaml";
+
+const Dataverse = ({ files }) => <div></div>;
+
+export default Dataverse;
