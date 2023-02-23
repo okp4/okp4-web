8d29bffd3015c96af8cd5103f2e9b1d10450c87f
ErikssonJoakim <j.eriksson@okp4.com>
2023-02-23 13:24:23 +0100

    feat(portal): init page

---
+++ okp4-gatsby/src/components/pages/explore/Portal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Halo from "../../animations/Halo.js";
+
+const Portal = () => {
+  return (
+    <div className="portal__main">
+      <Halo />
+    </div>
+  );
+};
+
+export default Portal;
