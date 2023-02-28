239192c1e31d1e7324293a652ca09ff998643717
Anja Miscevic <anja.miscevic@myy.haaga-helia.fi>
2023-02-28 10:05:41 +0100

    feat: add all the components

--- okp4-gatsby/src/components/pages/interact/Developers.js
+++ okp4-gatsby/src/components/pages/interact/Developers.js
@@ -4,6 +4,13 @@ import contentDevelopers from "../../../../content/pages/interact/developers.yam
 import contentSocials from "../../../../content/transversals/socials.yaml";
 import IconGithub from "../../../assets/images/socials/github.inline.svg";
 import IconDiscord from "../../../assets/images/socials/discord.inline.svg";
+import { StaticImage } from "gatsby-plugin-image";
+import IconArrowtr from "../../../assets/images/icons/arrow-tr.inline.svg";
+import DevDesign from "../../../assets/images/icons/developers/dev-design-icon.inline.svg";
+import DevEcosystem from "../../../assets/images/icons/developers/dev-ecosystem-icon.inline.svg";
+import DebIBC from "../../../assets/images/icons/developers/dev-ibc-icon.inline.svg";
+import DevResources from "../../../assets/images/icons/developers/dev-resources-icon.inline.svg";
+import DevWeb from "../../../assets/images/icons/developers/dev-web-bridges-icon.inline.svg";
 
 const Developers = () => {
   const devSocials = useMemo(
@@ -16,7 +23,38 @@ const Developers = () => {
       {
         icon: <IconDiscord />,
         title: contentSocials.discord.label,
-        url: contentSocials.discords.url,
+        url: contentSocials.discord.url,
+      },
+    ],
+    []
+  );
+
+  const devBlocks = useMemo(
+    () => [
+      {
+        icon: <DevEcosystem />,
+        title: contentDevelopers.blocks[0].title,
+        description: contentDevelopers.blocks[0].text,
+      },
+      {
+        icon: <DevDesign />,
+        title: contentDevelopers.blocks[1].title,
+        description: contentDevelopers.blocks[1].text,
+      },
+      {
+        icon: <DevResources />,
+        title: contentDevelopers.blocks[2].title,
+        description: contentDevelopers.blocks[2].text,
+      },
+      {
+        icon: <DevWeb />,
+        title: contentDevelopers.blocks[3].title,
+        description: contentDevelopers.blocks[3].text,
+      },
+      {
+        icon: <DebIBC />,
+        title: contentDevelopers.blocks[4].title,
+        description: contentDevelopers.blocks[4].text,
       },
     ],
     []
@@ -27,18 +65,50 @@ const Developers = () => {
       <Halo />
       <div className="wrapper">
         <div className="developers__intro">
-          <h1>{contentDevelopers.title}</h1>
-          <p>{contentDevelopers.description}</p>
-          {console.log(contentSocials)}
-          {/* {devSocials.map(({ icon, title, url }) => (
-            <a href={url} className="developers__intro__socials" target="_blank"
-            rel="noreferrer">
-              {icon}
-              <p>{title}</p>
-            </a>
-          ))} */}
-          <p>{contentSocials.github.label}</p>
+          <div className="developers__intro__left">
+            <h1>{contentDevelopers.title}</h1>
+            <p>{contentDevelopers.description}</p>
+            <div className="developers__intro__socials">
+              {devSocials.map(({ icon, title, url }) => (
+                <a
+                  href={url}
+                  className="developers__intro__socials__icon"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {icon}
+                  <span>
+                    <p className="developers__intro__socials__title">{title}</p>
+                    <IconArrowtr />
+                  </span>
+                </a>
+              ))}
+            </div>
+          </div>
+          <StaticImage
+            className="developers__intro__image"
+            src="../../../../content/images/developers-intro-laptop.png"
+            alt="developers OKP4"
+          />
         </div>
+        <div className="developers__blocks">
+          {devBlocks.map(({ title, description, icon }) => (
+            <div className="dev__block__container">
+              <div className="dev__block__wrapper">{icon} </div>
+              <div className="dev__block__text">
+                <p className="dev__blocks__title"> {title}</p>
+                <p>{description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="developers__halo" />
+        <div
+          className="developers__last__paragraph"
+          dangerouslySetInnerHTML={{
+            __html: contentDevelopers.paragraph,
+          }}
+        />
       </div>
     </div>
   );
