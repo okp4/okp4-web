fdfa83f6823c9960fc20ca4b3a73ebccc77d47ab
Anja Miscevic <anja.miscevic@myy.haaga-helia.fi>
2023-03-01 13:52:45 +0100

    feat(developers): hover effects, image style

--- okp4-gatsby/src/components/pages/interact/Developers.js
+++ okp4-gatsby/src/components/pages/interact/Developers.js
@@ -1,5 +1,4 @@
 import React, { useMemo } from "react";
-import Halo from "../../animations/Halo";
 import contentDevelopers from "../../../../content/pages/interact/developers.yaml";
 import contentSocials from "../../../../content/transversals/socials.yaml";
 import IconGithub from "../../../assets/images/socials/github.inline.svg";
@@ -11,6 +10,7 @@ import DevEcosystem from "../../../assets/images/icons/developers/dev-ecosystem-
 import DebIBC from "../../../assets/images/icons/developers/dev-ibc-icon.inline.svg";
 import DevResources from "../../../assets/images/icons/developers/dev-resources-icon.inline.svg";
 import DevWeb from "../../../assets/images/icons/developers/dev-web-bridges-icon.inline.svg";
+import classNames from "classnames";
 
 const Developers = () => {
   const devSocials = useMemo(
@@ -35,26 +35,31 @@ const Developers = () => {
         icon: <DevEcosystem />,
         title: contentDevelopers.blocks[0].title,
         description: contentDevelopers.blocks[0].text,
+        label: contentDevelopers.blocks[0].label,
       },
       {
         icon: <DevDesign />,
         title: contentDevelopers.blocks[1].title,
         description: contentDevelopers.blocks[1].text,
+        label: contentDevelopers.blocks[1].label,
       },
       {
         icon: <DevResources />,
         title: contentDevelopers.blocks[2].title,
         description: contentDevelopers.blocks[2].text,
+        label: contentDevelopers.blocks[2].label,
       },
       {
         icon: <DevWeb />,
         title: contentDevelopers.blocks[3].title,
         description: contentDevelopers.blocks[3].text,
+        label: contentDevelopers.blocks[3].label,
       },
       {
         icon: <DebIBC />,
         title: contentDevelopers.blocks[4].title,
         description: contentDevelopers.blocks[4].text,
+        label: contentDevelopers.blocks[4].label,
       },
     ],
     []
@@ -62,38 +67,55 @@ const Developers = () => {
 
   return (
     <div className="developers__main">
-      <Halo />
       <div className="wrapper">
         <div className="developers__intro">
           <div className="developers__intro__left">
             <h1>{contentDevelopers.title}</h1>
             <p>{contentDevelopers.description}</p>
-            <div className="developers__intro__socials">
-              {devSocials.map(({ icon, title, url }) => (
-                <a
-                  href={url}
-                  className="developers__intro__socials__icon"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {icon}
-                  <span>
-                    <p className="developers__intro__socials__title">{title}</p>
-                    <IconArrowtr />
-                  </span>
-                </a>
-              ))}
+            <div className="social__media">
+              <a
+                href={contentSocials.github.url}
+                className="socials__link github"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IconGithub />
+                <span>
+                  <p className="text">{contentSocials.github.label}</p>
+                  <IconArrowtr className="arrow" />
+                </span>
+              </a>
+
+              <a
+                href={contentSocials.discord.url}
+                className="socials__link discord"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <IconDiscord />
+                <span>
+                  <p className="text">{contentSocials.discord.label}</p>
+                  <IconArrowtr className="arrow" />
+                </span>{" "}
+              </a>
             </div>
           </div>
-          <StaticImage
-            className="developers__intro__image"
-            src="../../../../content/images/developers-intro-laptop.png"
-            alt="developers OKP4"
-          />
+          <div className="developers__intro__image__wrapper">
+            <StaticImage
+              className="developers__intro__image"
+              src="../../../../content/images/developers-intro-laptop.png"
+              alt="developers OKP4"
+            />
+            {/* <StaticImage
+              className="developers__logo__image"
+              src="../../../../content/images/developers-logo-okp4.png"
+              alt="logo OKP4"
+            /> */}
+          </div>
         </div>
         <div className="developers__blocks">
-          {devBlocks.map(({ title, description, icon }) => (
-            <div className="dev__block__container">
+          {devBlocks.map(({ title, description, icon, label }) => (
+            <div className={classNames("dev__block__container", label)}>
               <div className="dev__block__wrapper">{icon} </div>
               <div className="dev__block__text">
                 <p className="dev__blocks__title"> {title}</p>
@@ -102,6 +124,8 @@ const Developers = () => {
             </div>
           ))}
         </div>
+        <div className="background__image" />
+
         <div className="developers__halo" />
         <div
           className="developers__last__paragraph"
