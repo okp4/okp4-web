e9685973018d985696090841781c46e23024e769
Joey <joey.remion@gmail.com>
2023-03-02 18:26:57 +0100

    Revert "feat(contact): add recaptcha"
    This reverts commit 9bf65229942b50855d0e8e3e02d41f7be9eae688.

--- okp4-gatsby/src/components/pages/contact/Contact.js
+++ okp4-gatsby/src/components/pages/contact/Contact.js
@@ -4,12 +4,10 @@ import RightArrowIcon from "../../../assets/images/icons/arrow-mr.inline.svg";
 import ErrorIcon from "../../../assets/images/icons/error_circle_rounded.inline.svg";
 import MessageSentIcon from "../../../assets/images/icons/mark_email_read.inline.svg";
 import axios from "axios";
-import ReCAPTCHA from "react-google-recaptcha";
 import {
-  getSendinblueApiEndpoint,
-  getSendinblueApiKey,
-  getCaptchaSiteKey,
-} from "../../../utils/Utils";
+  SENDINBLUE_API_ENDPOINT,
+  SENDINBLUE_API_KEY,
+} from "gatsby-env-variables";
 
 const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -23,7 +21,6 @@ const Contact = () => {
   });
   const [isEmailValid, setEmailValid] = useState(true);
   const [formState, setFormState] = useState("notSubmitted");
-  const recaptchaRef = React.useRef(null);
 
   const checkEmailFormat = (email) => email.match(EMAIL_REGEX);
 
@@ -38,25 +35,24 @@ const Contact = () => {
   };
 
   const handleSubmit = useCallback(
-    async (event) => {
+    (event) => {
       event.preventDefault();
-
-      const token = await recaptchaRef.current.executeAsync();
-      recaptchaRef.current.reset();
-
-      // TODO : check if token is valid
-
+      console.log("handleSubmit", {
+        SENDINBLUE_API_ENDPOINT,
+        SENDINBLUE_API_KEY,
+      });
       const isEmailFormatValid = checkEmailFormat(formData.email);
       setEmailValid(isEmailFormatValid);
 
       if (!isEachMandatoryFieldFilled()) {
+        console.log("mandatoryFieldMissing");
         setFormState("mandatoryFieldMissing");
       } else if (isEmailFormatValid) {
         const config = {
           headers: {
             accept: "application/json",
             "content-type": "application/json",
-            "api-key": getSendinblueApiKey(),
+            "api-key": SENDINBLUE_API_KEY,
           },
         };
 
@@ -71,7 +67,7 @@ const Contact = () => {
         };
 
         axios
-          .post(getSendinblueApiEndpoint(), JSON.stringify(body), config)
+          .post(SENDINBLUE_API_ENDPOINT, JSON.stringify(body), config)
           .then((response) => {
             if (response.status === 400) {
               setFormState("error");
@@ -88,7 +84,7 @@ const Contact = () => {
           .catch((err) => setFormState("error"));
       }
     },
-    [formData, recaptchaRef]
+    [formData]
   );
 
   return (
@@ -170,20 +166,6 @@ const Contact = () => {
                   onChange={handleFormChange}
                 />
               </div>
-              <ReCAPTCHA
-                ref={recaptchaRef}
-                sitekey={getCaptchaSiteKey()}
-                size="invisible"
-                onChange={() => {
-                  console.log("Captcha onChange");
-                  const recaptchaValue = recaptchaRef.current.getValue();
-                  console.log({ recaptchaValue });
-                }}
-                onErrored={() => console.log("Captcha onErrored")}
-                onExpired={() => {
-                  console.log("Captcha onExpired");
-                }}
-              />
               <button onClick={handleSubmit}>
                 {content.send} <RightArrowIcon />
               </button>
