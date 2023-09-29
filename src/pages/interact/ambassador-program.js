b65ce1ccc9b735ed5d6d7fdb4906aa32985bb75b
lauren olivier <laurenzolivier@gmail.com>
2023-09-29 15:36:29 +0200

    feat(ambassador): implement ambassador page content

--- src/pages/interact/ambassador-program.js
+++ src/pages/interact/ambassador-program.js
@@ -4,21 +4,25 @@ import Layout from "../../components/Layout";
 import content from "/content/pages/interact/ambassador-program.yaml";
 import Seo from "../../components/SEO";
 import seoContent from "/content/transversals/seo.yaml";
+import AmbassadorProgram from "../../components/pages/interact/AmbassadorProgram";
 
-export default function AmbassadorProgram({ data }) {
+export default function Ambassador({ data }) {
+  console.log({ data });
+
   return (
     <Layout
-      location={"ambassador-program"}
+      location={"ambassador_program"}
       files={data}
       breadcrumbs={content.breadcrumbs}
       isStatic
       withDocs={false}
       withPartners={false}
       withAssociates={false}
-    ></Layout>
+    >
+      <AmbassadorProgram files={data} />
+    </Layout>
   );
 }
-
 export const pageQuery = graphql`
   query FilesQuery {
     allImages: allFile(filter: { extension: { nin: ["mp4", "ogg", "webm"] } }) {
