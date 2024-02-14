d616f829233f00f20e64d0d228c155919d1bd663
ccamel <camel.christophe@gmail.com>
2024-02-14 10:01:16 +0100

    ci(workflow): remove staging environment

--- .github/environments/staging/env.js
+++
@@ -1,5 +0,0 @@
-const OKP4_ENV = {
-  SENDINBLUE_API_KEY:
-    "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",
-  SENDINBLUE_API_ENDPOINT: "https://api.sendinblue.com/v3/contacts",
-};
